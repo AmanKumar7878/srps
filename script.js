512f7c91f6ef61484c7c529587b6e4caae419534
Aman Kumar <amankumardagur7878@gmail.com>
2023-04-02 15:05:44 +0530

    1.5

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function myFunc(){
+    var x = document.getElementById("navbar");
+    if(x.style.display =="block"){
+         x.style.display = "none";
+    }
+    else{
+        x.style.display = "block";
+    }
+}

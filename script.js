6031d1ba8adef8e6fcdb3eeb62f7aa1ace3191a5
Aman Kumar <amankumardagur7878@gmail.com>
2023-04-07 12:49:27 +0530

    1.7

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-
+var x = document.getElementById("lbl");
 function myFunc(){
-    var x = document.getElementById("navbar");
-    if(x.style.display =="none"){
-         x.style.display = "block";
+    console.log("Hrekllo");
+   // var x = document.getElementById("navbar");
+    if(x.style.display =="block"){
+         x.style.display = "none";
     }
     else{
-        x.style.display = "none";
+        x.style.display = "block";
     }
 }
+x.onclick = myFunc();
+
